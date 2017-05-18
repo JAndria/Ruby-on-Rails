546328d27660be89bfd39a82cd5862d1ae57f79e
Janelle <hilliard.jan@gmail.com>
2017-05-18 03:36:04 +0000

    5.2b Creating and styling forms

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,13 @@
 class Product < ActiveRecord::Base
     has_many :orders
+    
+    def self.search(search_term)
+      if Rails.env.production?
+        Product.where("name ilike ?", "%#{search_term}%")
+      else
+        Product.where("name LIKE ?", "%#{search_term}%")
+      end
+      
+    end
+    
 end
