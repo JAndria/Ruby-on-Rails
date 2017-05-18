a3fe485f001bad07a6ebfe5fb841a08c65c892e2
Janelle <hilliard.jan@gmail.com>
2017-05-18 05:14:29 +0000

    5.2c Fixed checking of rails env

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,10 +2,10 @@ class Product < ActiveRecord::Base
     has_many :orders
     
     def self.search(search_term)
-      if Rails.env.production?
-        Product.where("name ilike ?", "%#{search_term}%")
-      else
+      if Rails.env == "development"
         Product.where("name LIKE ?", "%#{search_term}%")
+      else
+        Product.where("name ilike ?", "%#{search_term}%")
       end
       
     end
