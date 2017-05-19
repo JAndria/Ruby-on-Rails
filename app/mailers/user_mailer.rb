fe95f8209e95bcf663fc5779a4fefd969b0dd085
Janelle <hilliard.jan@gmail.com>
2017-05-19 02:02:21 +0000

    5.3c Fixed the To email in user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ApplicationMailer
-  default from: "from@example.com"
+  default from: "TestShop@email.com"
 
   def contact_form(email, name, message)
   @message = message
     mail(from: email,
-         to: 'your-email@example.com',
+         to: 'hilliard.jan@gmail.com',
          subject: "A new contact form message from #{name}")
   end
 end
