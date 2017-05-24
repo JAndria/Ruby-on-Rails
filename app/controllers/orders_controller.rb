9709ab7c717d3859f2236466da69a092cf76dc90
Janelle <hilliard.jan@gmail.com>
2017-05-24 17:47:49 +0000

    5.5e Fixed page authorizations and minor view tweaking

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,9 @@
 class OrdersController < ApplicationController
   before_filter :authenticate_user!
+  load_and_authorize_resource
 
     def index
-      @orders = Order.all
+      @orders = Order.where("user_id = ?", current_user.id)
     end
     
     def show
@@ -15,6 +16,17 @@ class OrdersController < ApplicationController
     end
     
     def destroy
+    end
+    
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_order
+      @order = Order.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def order_params
+      params.require(:order).permit(:total)
     end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
     has_many :orders
-    
+
     def self.search(search_term)
       if Rails.env == "development"
         Product.where("name LIKE ?", "%#{search_term}%")
