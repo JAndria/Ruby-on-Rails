d3f6485f54bc9604cb721c4ad3c67f47318a425f
Janelle <hilliard.jan@gmail.com>
2017-05-25 14:43:57 +0000

    5.7b Created comment scope and methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
     has_many :orders
+    has_many :comments
 
     def self.search(search_term)
       if Rails.env == "development"
@@ -8,6 +9,14 @@ class Product < ActiveRecord::Base
         Product.where("name ilike ?", "%#{search_term}%")
       end
       
+    end
+    
+    def highest_rating_comment
+      comments.rating_desc.first
+    end
+    
+    def lowest_rating_comment
+      comments.rating_desc.last
     end
     
 end
