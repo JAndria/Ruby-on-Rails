e52907eaa6952e4f4c867fafc4f10bc3d0ea79f1
Janelle <hilliard.jan@gmail.com>
2017-06-03 03:36:00 +0000

    5.10c Added more restrictions and styling/cleanup

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,8 +3,17 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
-    can :read, Order, user_id: user.id
+    if user.admin?
+      can :manage, :all
+    else
+      can :manage, User, id: user.id
+      can :read, Order, user_id: user.id
+      can :read, Comment
+      cannot :destroy, Comment
+      can :read, Product
+      cannot [:update, :destroy], Product
+    end
+    
     
     
     # Define abilities for the passed in user here. For example:
