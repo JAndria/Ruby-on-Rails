d3f5f738bfe7d2e800e419eaaa8a507e4c1d0cf2
Janelle <hilliard.jan@gmail.com>
2017-06-28 19:42:31 +0000

    Redesign login process & test Stripe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,11 @@ class ApplicationController < ActionController::Base
     redirect_to main_app.root_url, alert: exception.message
   end
   
+  def after_sign_in_path_for(resource)
+    request.referrer
+  end
+  def after_sign_out_path_for(resource_or_scope)
+    request.referrer
+  end
+  
 end
