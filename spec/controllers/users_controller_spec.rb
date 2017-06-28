58b10ad1249972a9aa31abd092b71ad352641991
Janelle <hilliard.jan@gmail.com>
2017-06-28 02:14:22 +0000

    6.3 modify users_controller_spec.rb

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,38 +1,43 @@
 require 'rails_helper'
 
-describe UsersController, :type => :controller do
-  before do
-    @user1 = FactoryGirl.create(:user)
-    @user2 = FactoryGirl.create(:admin)
-  end
-
-  describe 'GET #show' do
-
-    context 'User is logged in' do
+describe UsersController, type: :controller do
+  
+  
+  user = FactoryGirl.create(:user)
+  user2 = FactoryGirl.create(:user)
+  
+  describe "GET #show" do
+    
+    context "when a user is logged in" do
       before do
-        sign_in @user1
+        sign_in user
       end
-
-      it 'loads correct user details' do
-        get :show, id: @user1.id
-        expect(response).to have_http_status(200)
-        expect(assigns(:user)).to eq @user1
+      
+      it "loads correct user details" do
+        get :show, id: user.id
+        expect(response).to be_ok
+        expect(assigns(:user)).to eq user
       end
-
-      it 'cannot access show page of user2' do
-        get :show, id: @user2.id
-        expect(response).to have_http_status(302)
-        expect(assigns(:user)).not_to eq @user1
+    end
+    
+    context "when a user is not logged in" do
+      it "redirects to login" do
+        get :show, id: user.id
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+    
+    context "when user tries to view user2's page" do
+      before do
+        sign_in user
+      end
+      
+      it "redirects to root" do
+        get :show, id: user2.id
         expect(response).to redirect_to(root_path)
       end
     end
-
-    context 'no user is logged in' do
-      it 'redirects to sign in' do
-        get :show, id: @user1.id
-        redirect_to(root_path)
-      end
-    end
-
+    
   end
+  
 end
